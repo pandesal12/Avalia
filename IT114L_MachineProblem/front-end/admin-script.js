42fc6757643d84ea0be662f1b1b290b1926f04be
Noela Mae <andosayella13@gmail.com>
2024-03-17 20:28:23 +0800

    added user in navbar

--- IT114L_MachineProblem/front-end/admin-script.js
+++ IT114L_MachineProblem/front-end/admin-script.js
@@ -1,1 +1,36 @@
-﻿
+﻿//Navigation Bar----------------------------------------------------------
+const nav = document.querySelector(".nav");
+    searchIcon = document.querySelector("#searchIcon"),
+    navOpenBtn = document.querySelector(".navOpenBtn"),
+    navCloseBtn = document.querySelector(".navCloseBtn");
+    userProf2 = document.querySelector(".userprofile-visible");
+
+    searchIcon.addEventListener("click", () => {
+        // sumisilip pa si edna, may error pa
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            userProf2.style.display = "none";
+        } else {
+            userProf2.style.display = "flex";
+        }
+
+        nav.classList.toggle("openSearch");
+        nav.classList.remove("openNav");
+        if (nav.classList.contains("openSearch")) {
+            searchIcon.classList.replace("uil-search", "uil-times")
+            if (userProf2.style.display === "flex") {
+                userProf2.style.display = "none";
+            }
+            return;
+        }
+        searchIcon.classList.replace("uil-times", "uil-search")
+    });
+
+    navOpenBtn.addEventListener("click", () => {
+        nav.classList.add("openNav");
+        nav.classList.remove("openSearch");
+        searchIcon.classList.replace("uil-times", "uil-search")
+
+    });
+    navCloseBtn.addEventListener("click", () => {
+        nav.classList.remove("openNav");
+    });
--- IT114L_MachineProblem/front-end/user-script.js
+++ IT114L_MachineProblem/front-end/user-script.js
@@ -1,27 +1,39 @@
 ﻿//Navigation Bar----------------------------------------------------------
 const nav = document.querySelector(".nav");
-searchIcon = document.querySelector("#searchIcon"),
+    searchIcon = document.querySelector("#searchIcon"),
     navOpenBtn = document.querySelector(".navOpenBtn"),
     navCloseBtn = document.querySelector(".navCloseBtn");
+    userProf2 = document.querySelector(".userprofile-visible");
 
-searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if (nav.classList.contains("openSearch")) {
-        return searchIcon.classList.replace("uil-search", "uil-times")
-    }
-    searchIcon.classList.replace("uil-times", "uil-search")
-});
+    searchIcon.addEventListener("click", () => {
+        // sumisilip pa si edna, may error pa
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            userProf2.style.display = "none";
+        } else {
+            userProf2.style.display = "flex";
+        }
 
-navOpenBtn.addEventListener("click", () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-    searchIcon.classList.replace("uil-times", "uil-search")
+        nav.classList.toggle("openSearch");
+        nav.classList.remove("openNav");
+        if (nav.classList.contains("openSearch")) {
+            searchIcon.classList.replace("uil-search", "uil-times")
+            if (userProf2.style.display === "flex") {
+                userProf2.style.display = "none";
+            }
+            return;
+        }
+        searchIcon.classList.replace("uil-times", "uil-search")
+    });
 
-});
-navCloseBtn.addEventListener("click", () => {
-    nav.classList.remove("openNav");
-});
+    navOpenBtn.addEventListener("click", () => {
+        nav.classList.add("openNav");
+        nav.classList.remove("openSearch");
+        searchIcon.classList.replace("uil-times", "uil-search")
+
+    });
+    navCloseBtn.addEventListener("click", () => {
+        nav.classList.remove("openNav");
+    });
 
 //Carousel----------------------------------------------------------
 document.addEventListener("DOMContentLoaded", function () {
